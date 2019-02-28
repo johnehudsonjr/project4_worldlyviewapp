9f580178eabf926fef60744066c110207fba5768
johnehudsonjr <jeh.hudson@hotmail.com>
2019-02-28 10:22:03 -0500

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Homepage from './components/Homepage';
+import Europe from './components/Europe'
+import Asia from './components/Asia'
+import Africa from './components/Africa'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <Router>
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/europe" component={Europe}/>
+            <Route exact path="/asia" component={Asia}/>
+            <Route exact path="/africa" component={Africa}/>
+          </Switch>
+        </Router>
       </div>
     );
   }
 }
 
 export default App;
+
